196cbdf90d03adb1012bce1d1718a7b7ddaa8f3e
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-21 18:32:31 -0500

    task router/model almost done

--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,22 @@
-// build your `/api/tasks` router here
-const express = require('express');
-// const helpers = require('./model');
+const express = require("express");
+const Task = require("./model");
 
 const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Task.find()
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Task.add(req.body)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch(next);
+});
+
 module.exports = router;
