6625f602c8b0d5515b0d9f60df0558c30f75e067
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-21 18:34:56 -0500

    formatting

--- api/task/model.js
+++ api/task/model.js
@@ -1,40 +1,46 @@
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
 
 async function find() {
-    const rows = await db('tasks as t')
-        .leftJoin('projects as p', 'p.project_id', 't.project_id')
-        .select(
-            't.task_id',
-            't.task_description',
-            't.task_notes',
-            't.task_completed',
-            'p.project_name',
-            'p.project_description'
-        );
-    const result = [];
+  const rows = await db("tasks as t")
+    .leftJoin("projects as p", "p.project_id", "t.project_id")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_name",
+      "p.project_description"
+    );
+  const result = [];
 
-    rows.forEach((row) => {
-        result.push({...row, task_completed: row.task_completed === 1 ? true : false,})
-    })
-    return result
+  rows.forEach((row) => {
+    result.push({
+      ...row,
+      task_completed: row.task_completed === 1 ? true : false,
+    });
+  });
+  return result;
 }
 
 async function add(task) {
-    const [id] =await db('task as t').insert(task)
+  const [id] = await db("task as t").insert(task);
 
-    const newTask = await db('tasks as t')
-    .leftJoin('projects as p', 'p.project_id', 't.project_id')
+  const newTask = await db("tasks as t")
+    .leftJoin("projects as p", "p.project_id", "t.project_id")
     .select(
-        't.task_id',
-        't.task_description',
-        't.task_notes',
-        't.task_completed',
-        'p.project_id'
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_id"
     )
-    .where('t.task_id', id)
+    .where("t.task_id", id)
     .first();
 
-    return {...newTask, task_completed: newTask.task_completed === 1 ? true : false,}
+  return {
+    ...newTask,
+    task_completed: newTask.task_completed === 1 ? true : false,
+  };
 }
 
-module.exports = { find, add }
+module.exports = { find, add };
